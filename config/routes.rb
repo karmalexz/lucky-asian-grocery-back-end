44a6e07c273fc4d6a192384c86050c196e56b9b3
Frankie Tsoi <email@example.com>
2022-02-20 16:43:31 +1100

    fixed password

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/products/:id/delete' => 'products#destroy', as: 'destroy_product'
   resources :products
   resources :categories
+  resources :cart
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
