0186a8da56f0e162a27cf8a9aeeeb342b5ed6c69
Frankie Tsoi <email@example.com>
2022-02-21 11:57:10 +1100

    gitpull

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     include Knock::Authenticable
 
 
-    before_action :authenticate_user
+    # before_action :authenticate_user #move this to separate controllers
     
     def current
         render json: current_user
