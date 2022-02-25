19f95e3ea75b52796a01228c1584acb1b8a66e57
Stanley Chin <stanleyshrink@hotmail.com>
2022-02-25 14:41:47 +1100

    Last Change

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -23,8 +23,7 @@ class OrdersController < ApplicationController
         #TODO:can delete cart_line_items for user after the status has been changed to paid
       end #end of each
 
-    end
-    #Shoudl get a panel that has a paynow and pass through the order ID to the backend and the form that takes the credit card number and happens before the pay now button and submit the cc number to the backend which we wont do but it will simulate what we need to do . First thing to beware of thee response that gets the line_tiem reponse server ID we need to save that state of the order_id and we submit that ID and that is for the final pay_id and that would change the stet from unpaid to paid . And if that works with the change from paid to unpiad, we can now go current_user.cart_line_items.destroy_all ONLY when the order has sucessfully updated the order from unpaid to paid. 
+    end 
 
     # STRIPE: And once the fake cc credit ards details are taken then we are ready to drop in stripe and then at least the db is working and we can finally implement that.
 
@@ -36,7 +35,7 @@ class OrdersController < ApplicationController
     order = Order.find params[:id]
     if order.update status: :paid
       order.save
-      render json: order, include :order_line_items
+      render json: order, include: :order_line_items
     else
       flash[:error] = "No Order Found"
     end
